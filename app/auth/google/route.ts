e4928e280db30dfe89337f83565021e5910faa59
Jahnvi <saxenajahnvi7@gmail.com>
2025-12-11 14:06:29 +0530

    feat(api): update Google authentication route to handle dynamic base URL

--- app/auth/google/route.ts
+++ app/auth/google/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.redirect("/api/auth/google", 308);
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const baseUrl = `${url.protocol}//${url.host}`;
+  return NextResponse.redirect(`${baseUrl}/api/auth/google`, 308);
 }
 
